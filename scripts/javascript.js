ad6d6bd6531bf1fd1a9a3253f5e398b760e157e3
YasmineHCMD <145350317+YasmineHCMD@users.noreply.github.com>
2024-10-01 09:28:45 +0200

    Add files via upload

---
+++ scripts/javascript.js
@@ -0,0 +1,20 @@
+console.log("Hello world!")
+
+const dropdown_knop = document.getElementById("dropdown-button");
+const dropdown_menu = document.getElementById("dropdown-menu");
+
+let stateMenu = false;
+
+function menuToggle(){
+    if(stateMenu == false){
+        dropdown_menu.style.display = 'flex';
+        stateMenu=true;
+        dropdown_knop.textContent = 'Minder ^';
+    } else {
+        dropdown_menu.style.display = 'none';
+        stateMenu=false;
+        dropdown_knop.textContent = 'Meer v'
+    }
+}
+
+dropdown_knop.addEventListener('click',menuToggle);
